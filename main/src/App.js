432c85c8286be7c10c5e5d7a5b3f6b9caca84ead
gaston59 <akihiro.ghf@gmail.com>
2020-01-22 21:21:43 +0900

    components

--- main/src/App.js
+++ main/src/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class App extends Component{
-  render(){
-    const input = 
- 
-        <React.Fragment>
+// class App extends Component{
+//   render(){
+//     const input = 
+//         <React.Fragment>
           
-          <label htmlFor = "bar">bar</label>
-          <input type="text" onChange={() => {console.log("I'm clicked!!")}}/>;
-        </React.Fragment>
-    return input;
+//           <label htmlFor = "bar">bar</label>
+//           <input type="text" onChange={() => {console.log("I'm clicked!!")}}/>;
+//         </React.Fragment>
+//     return input;
 
-  }
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+
+}
+const Cat = () => {
+  return <h1>myaoh</h1>
+
 }
 
 export default App;
