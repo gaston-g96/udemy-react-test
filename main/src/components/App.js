0a17f042105ab266008be992ca6b3930f54cf230
gaston59 <akihiro.ghf@gmail.com>
2020-01-25 14:52:57 +0900

    learn how to create count-app

--- main/src/components/App.js
+++ main/src/components/App.js
@@ -1,36 +1,27 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'
 
-const App = () => (<Counter></Counter>)
+import { increment, decrement } from '../actions'
+
 
 //class componentを作る。compontは継承して使う
-class　Counter extends Component{
-  constructor(props){
-    super(props)
-    this.state = { count:0 }
-    
-  }
-
-  handlePlusButton = () => {
-    console.log("+1 button pushed");
-    this.setState({ count: this.state.count + 1})
-  }
-
-  handleMinusButton = () => {
-    console.log("-1 button pushed");
-    this.setState({ count: this.state.count - 1})
-  }
-
-
+class　App extends Component{
   render(){
+    const props = this.props
+
     return(
       <React.Fragment>
-      <div>count: {this.state.count}</div>
-      <button onClick={this.handlePlusButton}>+1</button>
-      <button onClick={this.handleMinusButton}>-1</button>
+      <div>value: {props.value}</div>
+      <button onClick={props.increment}>+1</button>
+      <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
+const mapStateToProps = state =>({ value: state.count.value })
+const mapDispatchTeoProps =({increment, decrement})
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchTeoProps)(App)
+
+
--- main/src/reducers/count.js
+++ main/src/reducers/count.js
@@ -12,4 +12,4 @@ export default(state = initialState, action) => {
             return state
 
     }
-}
+} 
